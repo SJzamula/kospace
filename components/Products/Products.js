7850ccab4b769f6d3f54eaa3f4e4953fbff563e7
Zamula-Liuba <zamula.l08l@gmail.com>
2021-04-22 18:33:25 +0300

    kospace

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,9 +1,26 @@
 class Products {
 
     render() {
+        let htmlCatalog ='';
         CATALOG.forEach(({id, name, price, img }) => {
-            console.log(id, name, price, img );
+           
+            htmlCatalog += `
+            <li>
+                <span>${name}</span>
+                <img src="${img}"/>
+                <span>${price}</span>
+                <button>Buy</button>
+            </li>
+            `;
         })
+
+        const html = `
+            <ul>
+                ${htmlCatalog}
+            </ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
     }
 }
 
