aae00c88dfb15d18125eb0184545074682c86319
Zamula-Liuba <zamula.l08l@gmail.com>
2021-04-22 18:22:59 +0300

    kospace

---
+++ constants/catalog.js
@@ -0,0 +1,50 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Chapagetti',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/10/1325.970-1.jpg?resize=425%2C425&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el2',
+        name: 'Buldak Carbonara',
+        img: 'https://i2.wp.com/kospace.com.ua/wp-content/uploads/2020/10/13-1-%D0%9E%D1%81%D1%82%D1%80%D0%B0%D1%8F-%D0%BB%D0%B0%D0%BF%D1%88%D0%B0-Ramen-Carbonara-%D1%81-%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D1%8B%D0%BC-%D0%B2%D0%BA%D1%83%D1%81%D0%BE%D0%BC-130-%D0%B3.jpg?resize=425%2C425&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el3',
+        name: 'Shin Ramyun',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/09/183601496_images_16817247350.jpg?resize=425%2C425&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el4',
+        name: 'Chacharoni',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/10/-%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0-2020-11-24-%D0%BE-14.50.35-e1618435851639.png?resize=300%2C300&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el5',
+        name: 'Jin Ramen Mild',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/11/56-1-scaled-e1618436273791.jpg?resize=320%2C320&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el6',
+        name: 'Neoguri',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/10/183601524_images_16817247910-e1618436240953.jpg?resize=320%2C320&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el7',
+        name: 'Seafood Party',
+        img: 'https://i1.wp.com/kospace.com.ua/wp-content/uploads/2020/09/12-1-%D0%A1%D1%83%D0%BF-%D1%81-%D0%BB%D0%B0%D0%BF%D1%88%D0%BE%D0%B9-%D0%B8-%D0%BC%D0%BE%D1%80%D0%B5%D0%BF%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82%D0%B0%D0%BC%D0%B8-125-%D0%B3.jpg?resize=425%2C425&ssl=1',
+        price: 72,
+    },
+    {
+        id: 'el8',
+        name: 'Rabokki Noodle',
+        img: 'https://i0.wp.com/kospace.com.ua/wp-content/uploads/2020/11/61uFLe7OHPL._AC_SL1000_.jpg?resize=425%2C425&ssl=1',
+        price: 72,
+    },
+]
